8eaf1c8e514b336bef35437d9ba7c32544b27c3d
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-12-10 20:00:02 +0100

    Check if text is a string and set it as required thanks to prop-types

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './module';
 
 ReactDOM.render(
-  <App text="Lorem ipsum dolor sit amet" />,
+  <App text={"Lorem ipsum dolor sit amet"} />,
   document.getElementById("root")
 );
--- src/lib/SpongeBobTextComponent.js
+++ src/lib/SpongeBobTextComponent.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { spongeBobText } from './utils';
 
 const SpongeBobTextComponent = ({text}) => {
   return <>{spongeBobText(text)}</>
 }
+
+SpongeBobTextComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default SpongeBobTextComponent;
